785849ad97521b537abc24042a6bec5aef0fdb8b
Sarath Kumar Ezilarasan <sarathezil@gmail.com>
2025-10-29 19:04:18 +0530

    day-9

---
+++ day_9/Anchor.java
@@ -0,0 +1,20 @@
+package day_9;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Anchor {
+    public static void main(String[] args) {
+        String text = "appppp a ap";
+        String regex = "ap$";
+        // ^ -> beginning of the line
+        // $ -> end of the line
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+        while (matcher.find()) {
+            System.out.println("Found: " + matcher.group() + " at position " + matcher.start());
+        }
+    }
+}
---
+++ day_9/Quantifiers.java
@@ -0,0 +1,22 @@
+package day_9;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Quantifiers {
+    public static void main(String[] args) {
+        String text = "appppp a app";
+        String regex = "ap+";
+        //  {n} {n,} {n,m}
+        // ? -> 0 or 1 time {0,1}
+        // * -> 0 or more times {0,}
+        // + -> 1 or more times {1,}
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+        while (matcher.find()) {
+            System.out.println("Found: " + matcher.group() + " at position " + matcher.start());
+        }
+    }
+}
